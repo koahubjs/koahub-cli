d697199fc1ec3cbc00b291f51a1f13cf7fb2cd96
heqing <786699892@qq.com>
2016-12-27 17:29:18 +0800

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import child_process from "child_process";
 import shell from "shelljs";
 import program from "commander";
 import watch from "./util/watch.util";
-import {debug} from "./util/log.util";
+import { debug } from "./util/log.util";
 import config from "./config/index.config";
 import packageFile from "./../package.json";
 
@@ -21,7 +21,7 @@ program
     .option('-w, --watch', 'auto restart when a file is modified')
     .option('-c, --compile', 'auto babel process when a file is modified')
     .option('-r, --runtime [dir]', 'Babel compile and start the dir')
-    .action(function (script, options) {
+    .action(function(script, options) {
 
         const rootPath = process.cwd();
         const appName = path.dirname(script) || config.app;
@@ -31,19 +31,22 @@ program
         const runtimePath = path.resolve(rootPath, runtimeName);
         const runtimeFile = path.resolve(rootPath, script.replace(`${appName}`, `${runtimeName}`));
 
+        const binPath = path.resolve(process.mainModule.filename, '../../');
+        const babel = path.resolve(binPath, 'node_modules/.bin/babel');
+
         // 监控启动
         if (options.watch) {
 
             // 编译并且监控启动
             if (options.compile) {
-                shell.exec(path.normalize(`./node_modules/.bin/babel ${appName} --out-dir ${runtimeName}`));
+                shell.exec(path.normalize(`${babel} ${appName} --out-dir ${runtimeName}`));
             }
 
             let runtimeProcess;
 
             function startRuntimeProcess(runtimeFile) {
                 runtimeProcess = child_process.fork(runtimeFile);
-                runtimeProcess.on('exit', function (code, signal) {
+                runtimeProcess.on('exit', function(code, signal) {
                     if (runtimeProcess.connected == false) {
                         process.exit();
                     }
@@ -58,31 +61,31 @@ program
             startRuntimeProcess(runtimeFile);
 
             // 捕获SIGTERM退出信号
-            process.on('SIGTERM', function () {
+            process.on('SIGTERM', function() {
                 stopRuntimeProcess();
                 process.exit();
             });
 
             // 捕获未知错误
-            process.on('uncaughtException', function (err) {
+            process.on('uncaughtException', function(err) {
                 debug(err);
             });
 
             let time = new Date();
             let files = [];
             // 开启文件监控
-            watch(appName, runtimeName, function (filePath, compile = true) {
+            watch(appName, runtimeName, function(filePath, compile = true) {
 
                 if (options.compile == true && compile == true) {
                     let fileRuntimePath = filePath.replace(`${appName}`, `${runtimeName}`);
-                    files.push({filePath: filePath, fileRuntimePath: fileRuntimePath});
+                    files.push({ filePath: filePath, fileRuntimePath: fileRuntimePath });
                 }
 
                 let newTime = new Date();
-                let timeOut = setTimeout(function () {
+                let timeOut = setTimeout(function() {
                     if (files.length) {
                         for (let key in files) {
-                            shell.exec(path.normalize(`./node_modules/.bin/babel ${files[key].filePath} --out-file ${files[key].fileRuntimePath}`));
+                            shell.exec(path.normalize(`${babel} ${files[key].filePath} --out-file ${files[key].fileRuntimePath}`));
                         }
                         // 未编译文件清空
                         files = [];
@@ -105,7 +108,7 @@ program
 
         // 直接编译启动
         if (options.compile) {
-            shell.exec(path.normalize(`./node_modules/.bin/babel ${appName} --out-dir ${runtimeName}`));
+            shell.exec(path.normalize(`${babel} ${appName} --out-dir ${runtimeName}`));
         }
 
         // 直接启动
@@ -115,10 +118,10 @@ program
 program
     .command('controller [file]')
     .description('koahub create controller')
-    .action(function (file) {
+    .action(function(file) {
 
         const destFile = path.normalize(`${file}.controller.js`);
-        const srcFile = path.normalize(path.resolve(process.mainModule.filename, '../../', 'template/controller/index.controller.js'));
+        const srcFile = path.normalize(path.resolve(binPath, 'template/controller/index.controller.js'));
 
         fileCopySync(srcFile, destFile);
     });
@@ -126,7 +129,7 @@ program
 program
     .command('create [project]')
     .description('koahub create project')
-    .action(function (project) {
+    .action(function(project) {
 
         shell.exec('git clone https://github.com/einsqing/koahubjs-demo.git');
         fs.renameSync(path.resolve('koahubjs-demo'), path.resolve(project));
